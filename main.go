f47d1ceb6dbc8116125065e3754939b48b657cc2
Vijaya Donthireddy <donmac@vijayas-mbp.lan>
2018-09-27 21:51:27 -0700

    works with both full or relative path

--- main.go
+++ main.go
@@ -1,83 +1,92 @@
 package main
 
 import (
-    "archive/zip"
-    "fmt"
-    "io"
-    "os"
-    "path/filepath"
+	"strings"
+	"path"
+	"archive/zip"
+	"fmt"
+	"io"
+	"os"
+	"path/filepath"
 )
 
 func main() {
+    codePath := strings.Split(os.Args[1], "=")[1] + "/"
+    compress(codePath)
+}
 
-    files, err := listFiles("./folder")
-    if err != nil {
-        panic(err)
-    }
+func compress(codePath string) {
+	files, err := listFiles(codePath)
+	if err != nil {
+		panic(err)
+	}
 
-    zipMe(files, "test.zip")
+    zipFileName := path.Base(codePath) + ".zip"
 
-    for _, f := range files {
-        fmt.Println(f)
-    }
-    fmt.Println("Done!")
+    zipMe(files, path.Dir(codePath), zipFileName)
+    
+	// for _, f := range files {
+	// 	fmt.Println(f)
+	// }
+	fmt.Println("Done!")
 }
 
 func listFiles(root string) ([]string, error) {
-    var files []string
+	var files []string
 
-    err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
-        if !info.IsDir() {
-            files = append(files, path)
-        }
-        return nil
-    })
-    if err != nil {
-        return nil, err
-    }
-    return files, nil
-
-}
-
-func zipMe(filepaths []string, target string) error {
-
-    flags := os.O_WRONLY | os.O_CREATE | os.O_TRUNC
-    file, err := os.OpenFile(target, flags, 0644)
-
-    if err != nil {
-        return fmt.Errorf("Failed to open zip for writing: %s", err)
-    }
-    defer file.Close()
-
-    zipw := zip.NewWriter(file)
-    defer zipw.Close()
-
-    for _, filename := range filepaths {
-        if err := addFileToZip(filename, zipw); err != nil {
-            return fmt.Errorf("Failed to add file %s to zip: %s", filename, err)
-        }
-    }
-    return nil
+	err := filepath.Walk(root, func(path string, info os.FileInfo, err error) error {
+		if !info.IsDir() {
+			files = append(files, path)
+		}
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+	return files, nil
 
 }
 
-func addFileToZip(filename string, zipw *zip.Writer) error {
-    file, err := os.Open(filename)
+func zipMe(filepaths []string,  removePath string, target string) error {
 
-    if err != nil {
-        return fmt.Errorf("Error opening file %s: %s", filename, err)
-    }
-    defer file.Close()
+	flags := os.O_WRONLY | os.O_CREATE | os.O_TRUNC
+	file, err := os.OpenFile(target, flags, 0644)
 
-    wr, err := zipw.Create(filename)
-    if err != nil {
+	if err != nil {
+		return fmt.Errorf("Failed to open zip for writing: %s", err)
+	}
+	defer file.Close()
 
-        return fmt.Errorf("Error adding file; '%s' to zip : %s", filename, err)
-    }
+	zipw := zip.NewWriter(file)
+	defer zipw.Close()
 
-    if _, err := io.Copy(wr, file); err != nil {
-        return fmt.Errorf("Error writing %s to zip: %s", filename, err)
-    }
+	for _, filename := range filepaths {
+		if err := addFileToZip(filename, removePath, zipw); err != nil {
+			return fmt.Errorf("Failed to add file %s to zip: %s", filename, err)
+		}
+	}
+	return nil
+}
 
-    return nil
+func addFileToZip(filename string, removePath string, zipw *zip.Writer) error {
+	file, err := os.Open(filename)
+
+	if err != nil {
+		return fmt.Errorf("Error opening file %s: %s", filename, err)
+	}
+	defer file.Close()
+
+    filename = strings.Replace(filename, removePath, "", -1)
+
+	wr, err := zipw.Create(filename)
+	if err != nil {
+
+		return fmt.Errorf("Error adding file; '%s' to zip : %s", filename, err)
+	}
+
+	if _, err := io.Copy(wr, file); err != nil {
+		return fmt.Errorf("Error writing %s to zip: %s", filename, err)
+	}
+
+	return nil
 }
